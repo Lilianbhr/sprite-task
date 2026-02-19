5ba70e1483c33533e2ad50700b733f99d434abe7
Lilian <lil.bouhier@gmail.com>
2026-02-19 22:22:35 +0100

    Ajout du changement d'état au clic sur le bouton.

--- src/core.py
+++ src/core.py
@@ -7,10 +7,14 @@ from list_interface import TaskList
 class Core:
     def __init__(self, screen: pygame.Surface):
         self.screen = screen
-        self.surface_size = (2 * self.screen.get_width() // 3,
-                             2 * self.screen.get_height() // 3)
-        self.surface_pos = (self.screen.get_width()//2-self.surface_size[0]//2,
-                            self.screen.get_height()//2-self.surface_size[1]//2)
+        self.surface_size = (
+            2 * self.screen.get_width() // 3,
+            2 * self.screen.get_height() // 3
+        )
+        self.surface_pos = (
+            self.screen.get_width() // 2 - self.surface_size[0] // 2,
+            self.screen.get_height() // 2 - self.surface_size[1] // 2
+        )
         self.current_mode = Choose(self.surface_size, self.surface_pos)
         self.active_input = ""
 
@@ -25,4 +29,12 @@ class Core:
     def update(self):
         ret = self.current_mode.update(self.active_input)
         if ret:
-            print(ret)
+            size = (
+                4 * self.screen.get_width() // 5,
+                4 * self.screen.get_height() // 5
+            )
+            pos = (
+                self.screen.get_width() // 2 - size[0] // 2,
+                self.screen.get_height() // 2 - size[1] // 2
+            )
+            self.current_mode = TaskList(size, pos)
--- src/list_interface.py
+++ src/list_interface.py
@@ -1,9 +1,15 @@
-class TaskList:
-    def __init__(self):
+import pygame
+
+import general
+
+
+class TaskList(general.Div):
+    def __init__(self, size: tuple, pos: tuple):
+        super().__init__(size, pos)
         pass
 
     def update(self, active_input: str):
         pass
 
-    def display(self):
-        pass
+    def display(self, screen: pygame.Surface):
+        screen.blit(self.surface, self.hit_box)
