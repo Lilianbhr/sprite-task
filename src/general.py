db05725afafa5971ba8612ee1c5752d852de7f24
Lilian <lil.bouhier@gmail.com>
2026-02-26 17:45:57 +0100

    Nettoyage

--- src/general.py
+++ src/general.py
@@ -1,38 +1,6 @@
 import pygame
 
 
-class Button:
-    def __init__(self, name: str, dest: str, center: tuple, color: tuple):
-        self.screen_text = get_screen_text_for(name, 24)
-        self.destination = dest
-        self.center = center
-        self.screen_rect = self.screen_text.get_rect()
-        self.screen_rect.center = self.center
-        self.hit_box = self.add_padding(self.screen_rect, 10)
-        self.color = color
-
-    def collided(self, mouse_pos: tuple[int, int]) -> bool:
-        if self.hit_box.collidepoint(mouse_pos):
-            return True
-        return False
-
-    def get_destination(self):
-        return self.destination
-
-    @staticmethod
-    def add_padding(rect: pygame.Rect, padding: int) -> pygame.Rect:
-        res = rect.__copy__()
-        res.width += padding * 2
-        res.height += padding * 2
-        res.top -= padding
-        res.left -= padding
-        return res
-
-    def display(self, screen: pygame.Surface):
-        pygame.draw.rect(screen, self.color, self.hit_box)
-        screen.blit(self.screen_text, self.screen_rect)
-
-
 class Div:
     def __init__(self, size: tuple, pos: tuple):
         self.surface = pygame.Surface(size)
