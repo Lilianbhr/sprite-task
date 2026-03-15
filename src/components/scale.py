6d2654741f375f6c2a064083f047a2f7adbb009e
Lilian <lil.bouhier@gmail.com>
2026-03-15 21:51:19 +0100

    Ajout des autres champs de sélection dans FilterEditor

--- src/components/scale.py
+++ src/components/scale.py
@@ -4,10 +4,12 @@ from src.components.general import Div
 
 class Scale(Div):
     """ Permet la séléction d'une valeur dans [1;max_value] """
-    def __init__(self, size: tuple, pos: tuple, max_value: int, value=1):
+    def __init__(self, size: tuple, pos: tuple, max_value: int,
+                 value=1, direction=1):
         super().__init__(size, pos)
         self.max_value = max_value
         self.value = value
+        self.direction = direction
         self.gap = size[0] // 50
 
         self.elements = []
@@ -26,7 +28,7 @@ class Scale(Div):
 
             # Rendu visuel en fonction de la valeur actuelle
             boolean = False
-            if value <= self.value:
+            if value * self.direction <= self.value * self.direction:
                 boolean = True
 
             # Ajout des éléments à la liste
--- src/interfaces/filter_editor.py
+++ src/interfaces/filter_editor.py
@@ -1,6 +1,7 @@
 import pygame
 
 from src.components.general import Div, Button
+from src.components.scale import Scale
 
 
 class FilterEditor(Div):
@@ -23,6 +24,40 @@ class FilterEditor(Div):
             "quitter"
         )
 
+        scales_size = (size[0], size[1] // 20)
+
+        # Diff_min
+        self.diff_min = Scale(
+            scales_size,
+            (0, size[1] // 3),
+            5
+        )
+
+        # Diff_max
+        self.diff_max = Scale(
+            scales_size,
+            (0, self.diff_min.hit_box.bottom + 10),
+            5,
+            value=5,
+            direction=-1
+        )
+
+        # Long_min
+        self.long_min = Scale(
+            scales_size,
+            (0, self.diff_max.hit_box.bottom + 30),
+            5
+        )
+
+        # Long_max
+        self.long_max = Scale(
+            scales_size,
+            (0, self.long_min.hit_box.bottom + 10),
+            5,
+            value=5,
+            direction=-1
+        )
+
     def update(self, pos: tuple) -> str:
         if self.save.is_under(pos):
             return "enregistrer"
@@ -30,10 +65,32 @@ class FilterEditor(Div):
         elif self.quit.is_under(pos):
             return "quitter"
 
+        elif self.diff_min.is_under(pos):
+            s_pos = self.diff_min.get_relative_pos(pos)
+            self.diff_min.update(s_pos)
+
+        elif self.diff_max.is_under(pos):
+            s_pos = self.diff_max.get_relative_pos(pos)
+            self.diff_max.update(s_pos)
+
+        elif self.long_min.is_under(pos):
+            s_pos = self.long_min.get_relative_pos(pos)
+            self.long_min.update(s_pos)
+
+        elif self.long_max.is_under(pos):
+            s_pos = self.long_max.get_relative_pos(pos)
+            self.long_max.update(s_pos)
+
         else:
             return ""
 
     def display(self, screen: pygame.surface):
         self.save.display(self.surface)
         self.quit.display(self.surface)
+
+        self.diff_min.display(self.surface)
+        self.diff_max.display(self.surface)
+        self.long_min.display(self.surface)
+        self.long_max.display(self.surface)
+
         screen.blit(self.surface, self.hit_box)
