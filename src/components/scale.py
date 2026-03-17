0383c0864ee239cf244252b885359c74f02ab43f
Lilian <lil.bouhier@gmail.com>
2026-03-17 16:31:37 +0100

    DoubleScale

--- src/components/scale.py
+++ src/components/scale.py
@@ -5,13 +5,14 @@ from src.components.general import Div
 class Scale(Div):
     """ Permet la séléction d'une valeur dans [1;max_value] """
     def __init__(self, size: tuple, pos: tuple, max_value: int,
-                 value=1, direction=1):
+                 value=1, direction=1, limit=6):
         super().__init__(size, pos)
 
-        # Valuers
+        # Values
         self.max_value = max_value
         self.value = value
         self.direction = direction
+        self.limit = limit
 
         # Visuel
         self.gap = size[0] // 50
@@ -30,16 +31,18 @@ class Scale(Div):
         for value in range(1, self.max_value + 1):
 
             # Rendu visuel en fonction de la valeur actuelle
-            boolean = False
+            active = 0
             if value * self.direction <= self.value * self.direction:
-                boolean = True
+                active = 1
+            elif value * self.direction > self.limit * self.direction:
+                active = -1
 
             # Ajout des éléments à la liste
             self.elements.append(
                 Graduation(
                     (grad_width, self.hit_box.height),
                     (x, 0),
-                    boolean,
+                    active,
                     value
                 )
             )
@@ -51,6 +54,8 @@ class Scale(Div):
         for elt in self.elements:
             if elt.is_under(rel_pos):
                 self.value = elt.value
+                if self.value * self.direction >= self.limit * self.direction:
+                    self.value = self.limit
         self.set_elements()
 
     def display(self, screen: pygame.Surface):  # -------------------
@@ -61,16 +66,65 @@ class Scale(Div):
 # ============================================================================
 
 
+class DoubleScale(Div):
+    def __init__(self, size: tuple, pos: tuple, range_size: int, mini: int, maxi: int):
+        super().__init__(size, pos)
+        self.range_size = range_size
+        self.gap = size[1] // 10
+
+        self.min_scale = Scale(
+            (size[0], size[1] // 2),
+            (0, 0),
+            range_size,
+            value=mini,
+            limit=maxi
+        )
+        self.max_scale = Scale(
+            (size[0], size[1] // 2),
+            (0, self.min_scale.hit_box.bottom + self.gap),
+            range_size,
+            value=maxi,
+            direction=-1,
+            limit=mini
+        )
+
+    def set_limits(self, mini, maxi):
+        self.min_scale.limit = maxi
+        self.max_scale.limit = mini
+
+        self.min_scale.set_elements()
+        self.max_scale.set_elements()
+
+    def update(self, pos):
+        if self.min_scale.is_under(pos):
+            m_pos = self.min_scale.get_relative_pos(pos)
+            self.min_scale.update(m_pos)
+
+        elif self.max_scale.is_under(pos):
+            m_pos = self.max_scale.get_relative_pos(pos)
+            self.max_scale.update(m_pos)
+
+        self.set_limits(self.min_scale.value, self.max_scale.value)
+
+    def display(self, screen: pygame.Surface):
+        self.min_scale.display(self.surface)
+        self.max_scale.display(self.surface)
+        screen.blit(self.surface, self.hit_box)
+# ============================================================================
+
+
 class Graduation(Div):
     """ Élément qui constitue Scale """
-    def __init__(self, size: tuple, pos: tuple, active: bool, value: int):
+    def __init__(self, size: tuple, pos: tuple, active: int, value: int):
         super().__init__(size, pos)
         self.active = active
         self.value = value
         self.set_color()
 
     def set_color(self):  # -----------------------------------------
-        if self.active:
+        if self.active < 0:
+            self.surface.fill((50, 50, 50))
+        elif self.active:
             self.surface.fill((100, 0, 200))
         else:
             self.surface.fill((100, 100, 100))
